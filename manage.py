678caef3a0a4c76879a635b3ee727090bc0ee8be
Ruslan CHavus <chruslan4@gmail.com>
2024-06-14 12:06:11 +0300

    Removed comments

--- manage.py
+++ manage.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python
 import os
 import sys
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-
-# app = Flask(__name__)
-
-# # Configure the MySQL database connection
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://app_user:1234@172.17.0.2/app_db'
-# db = SQLAlchemy(app)
 
 
 if __name__ == "__main__":
--- todolist/settings.py
+++ todolist/settings.py
@@ -62,10 +62,6 @@ WSGI_APPLICATION = "todolist.wsgi.application"
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    # }
 
     'default': {
         'ENGINE': 'mysql.connector.django',
